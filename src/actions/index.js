ed178799349883469145a7bbc2970bc4d24d4368
jerostax <jeremy.geneste@gmail.com>
2019-05-15 11:03:03 +0200

    [ CONFIG ] Redux thunk middleware for fetchPosts action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,11 @@
+import jsonPlaceholder from '../api/jsonPlaceholder';
+
 export const fetchPosts = () => {
-  return {
-    type: 'FETCH_POST'
-  };
+  return function(dispatch, getState) {
+    const promise =  jsonPlaceholder.get('/posts');
+    return {
+      type: 'FETCH_POST',
+      payload: promise
+    };
+  } 
 };
